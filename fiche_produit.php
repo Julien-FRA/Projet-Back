31ad2535c1ab12b42e7e3350e606a6aceb28e018
FRANCOIS Julien <julien.francois@hetic.net>
2021-03-08 15:50:45 +0100

    fiche produit

--- fiche_produit.php
+++ fiche_produit.php
@@ -3,21 +3,24 @@
     <?php include 'config/template/nav.php'; ?>
 </header>
 <main>
-<section class="info_produit">
-    <img src="asset/img/virus.png" alt="photo article virus" class="article-pictures"/>
-    <article class="desc_produit">
-        <h4>Gameover Zeus</h4>
-        <p>$499</p>
-        <p>Ce terme générique désigne la capacité d’un programme 
-informatique à infecter plusieurs fichiers sur un ordinateur.
- À la manière d’une maladie virale, ils se propagent sur
- le web et d’une machine à une autre par le biais d’une 
-d’une intervention humaine. Que ce soit par e-mail, 
-par téléchargement de fichier ou via des supports physiques
- (clés USB, CD…).</p>
-        <a href="" class="discover-button button-fx">ACHETEZ</a>
-    </article>
-</section>
+    <section class="info_produit">
+        <div class="photo_produit">
+            <img src="asset/img/virus.png" alt="photo article virus" class=""/>
+        </div>
+        <article class="desc_produit">
+            <h4>Gameover Zeus</h4>
+            <p class="price">$499</p>
+            <p class="desc_text">Ce terme générique désigne la capacité d’un programme 
+    informatique à infecter plusieurs fichiers sur un ordinateur.
+    À la manière d’une maladie virale, ils se propagent sur
+    le web et d’une machine à une autre par le biais d’une 
+    d’une intervention humaine. Que ce soit par e-mail, 
+    par téléchargement de fichier ou via des supports physiques
+    (clés USB, CD…).</p>
+            <input type="button" value="Achetez" class="btn_achat">
+        </article>
+    </section>
+    section>
 </main>
 <hr>
 <?php include 'config/template/footer.php'; ?>
