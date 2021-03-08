920f160e663e5736e747e29b7a18b4a0e16ef22d
FRANCOIS Julien <julien.francois@hetic.net>
2021-03-08 13:30:13 +0100

    Home Page

--- index.php
+++ index.php
@@ -3,7 +3,98 @@
     <?php include 'config/template/nav.php'; ?>
 </header>
 <main>
-<h2 class="">Page Accueil</h2>
+<!-- <h2 class="">Page Accueil</h2> -->
+
+<section class="section_hero">
+</section>
+
+<section class="section_article">
+<figure>
+<figcaption>Virus</figcaption>
+<div class="product">
+            <img
+              src="asset/img/virus.png"
+              alt="photo article virus"
+              class="article-pictures"
+            />
+            <img
+              src="asset/img/star.png"
+              alt="notation produit"
+            />
+            <p class="price">$499</p>
+            <a
+            href=""
+            class="discover-button button-fx"
+            >DECOUVRIR</a
+          >
+</div>
+          </figure>
+          <figure>
+          <figcaption>Vers</figcaption>
+          <div class="product">
+            <img
+              src="asset/img/vers.png"
+              alt="photo article vers"
+              class="article-pictures"
+            />
+            <img
+              src="asset/img/star.png"
+              alt="notation produit"
+            />
+            <p class="price">$499</p>
+            <a
+            href=""
+            class="discover-button button-fx"
+            >DECOUVRIR</a
+          >
+          </div>
+          </figure>
+          <figure>
+          <figcaption>Spyware</figcaption>
+          <div class="product">
+            <img
+              src="asset/img/spyware.png"
+              alt="photo article spyware"
+              class="article-pictures"
+            />
+            <img
+              src="asset/img/star.png"
+              alt="notation produit"
+            />
+            <p class="price">$499</p>
+            <a
+            href=""
+            class="discover-button button-fx"
+            >DECOUVRIR</a
+          >
+          </div>
+          </figure>
+          <figure>
+          <figcaption>Ransomeware</figcaption>
+          <div class="product">
+            <img
+              src="asset/img/ransomware.png"
+              alt="photo article ransomeware"
+              class="article-pictures"
+            />
+            <img
+              src="asset/img/star.png"
+              alt="notation produit"
+            />
+            <p class="price">$499</p>
+            <a
+            href=""
+            class="discover-button button-fx"
+            >DECOUVRIR</a
+          >
+            </div>
+          </figure>
+</section>
+
+<section class="aboutus_section">
+<h3 class="about_title">A propos de nous</h3>
+</section>
+
 </main>
 <hr>
 <?php include 'config/template/footer.php'; ?>
