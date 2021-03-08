eaf3d53b074db32009e17496652f82710079ce08
FRANCOIS Julien <julien.francois@hetic.net>
2021-03-08 21:39:24 +0100

    Sign/Sign-up

--- inscription.php
+++ inscription.php
@@ -3,7 +3,18 @@
     <?php include 'config/template/nav.php'; ?>
 </header>
 <main>
-<h2 class="">Page inscription</h2>
+<section class="sign">
+        <form action="" class="signup">
+            <h1>Inscrivez-vous</h1>
+            <input type="name" name="name" id="name" class="inpt" required="required" placeholder="Your name">      
+            <input type="email" name="email" id="email" class="inpt" required="required" placeholder="Your email">
+            <input type="password" name="password" id="password" class="inpt" required="required" placeholder="Your password">
+            <div class="submit-wrap">
+                <input type="submit" value="Envoyer" class="submit">
+                <a href="#" class="more">Termes et conditions</a>
+            </div>
+        </form>
+</section>
 </main>
 <hr>
 <?php include 'config/template/footer.php'; ?>
