47129443ba572c3dafd797ba91cde9be69a870f0
Louis <louis.charles@hetic.net>
2021-03-09 11:17:52 +0100

    profil

--- profil.php
+++ profil.php
@@ -3,7 +3,42 @@
     <?php include 'config/template/nav.php'; ?>
 </header>
 <main>
-<h2 class="">Page profil</h2>
+<section class="profil_section">
+<article_one class="profil_nav">
+<figure class="user_figure">
+            <img
+              src="asset/img/avatar.jpg"
+              alt="photo de profil de l'utilisateur"
+              class="user_pictures"
+            />
+            <figcaption class="user_name">Bonjour, Utilisateur1</figcaption>
+          </figure>
+          <input type="button" value="Apercu du compte" class="btn_profil">
+          <input type="button" value="Mes commandes" class="btn_profil">
+          <input type="button" value="Modifier la photo" class="btn_profil">
+          <input type="button" value="Modifier le mot de passe" class="btn_profil">
+</article_one>
+<article_two class="profil_information">
+    <h2 class="profil_title">Mes informations</h2>
+
+    <div class="information_container">
+    <form action="" method="get" class="form_information">
+  <div class="form_contain">
+    <label for="name">PRENOM: </label>
+    <input type="text" name="firstname" id="firstname">
+  </div>
+  <div class="form_contain">
+    <label for="name">NOM: </label>
+    <input type="text" name="name" id="name">
+  </div>
+  <div class="form_contain">
+    <label for="email">ADRESSE E-MAIL: </label>
+    <input type="email" name="email" id="email">
+  </div>
+</form>
+</div>
+</article_two>
+</section>
 </main>
 <hr>
 <?php include 'config/template/footer.php'; ?>
