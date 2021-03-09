785bb53e7f6ef0fb70b38eaafced7d9a0b614508
FRANCOIS Julien <julien.francois@hetic.net>
2021-03-09 10:34:36 +0100

    new footer

--- config/template/footer.php
+++ config/template/footer.php
@@ -1,76 +1,24 @@
 <footer>
 <nav class="footer-nav">
-        <article class="about-footer">
-          <h2>
-            <a class="nav-fx" href=""
-              >A PROPOS</a
-            >
-          </h2>
-          <ul>
-            <li>Nos engagements</li>
-            <li>Mentions légales</li>
-            <li>Politique de confidentialité</li>
-          </ul>
-        </article>
-        <article class="customer-footer">
-          <h2>
-            <a class="nav-fx" href=""
-              >SERVICE CLIENT</a
-            >
-          </h2>
-          <ul>
-            <li>Nous contacter</li>
-            <li>FAQ</li>
-            <li>Condition générales</li>
-          </ul>
-        </article>
-        <article class="contact-footer">
-          <h2>
-            <a class="nav-fx" href=""
-              >CONTACT</a
-            >
-          </h2>
-          <ul>
-            <li>loky@outlook.com</li>
-          </ul>
-          <ul>
-            <li>03 54 85 29 55</li>
-          </ul>
-        </article>
-      </nav>
-
-      <div class="footer-icons">
-        <div class="social-groups">
-          <img
-            src="asset/img_footer/instagram.svg"
-            alt="logo d'instagram"
-            class="social-logo"
-          />
-          <img
-            src="asset/img_footer/twitter-sign.svg"
-            alt="logo twitter-sign"
-            class="social-logo"
-          />
-          <img
-            src="asset/img_footer/facebook.svg"
-            alt="logo facebook"
-            class="social-logo"
-          />
-        </div>
-
-        <div class="secure-groups">
-          <img
-            src="asset/img_footer/insurance.svg"
-            alt="logo assurance"
-            class="secure-logo"
-          />
-          <img
-            src="asset/img_footer/mastercard.svg"
-            alt="logo mastercard"
-            class="secure-logo"
-          />
-          <img src="asset/img_footer/visa.svg" alt="logo visa" class="secure-logo" />
-        </div>
+      <div class="footer_card">
+        <h4>Paiement sécurisé</h4>
+        <p>Vos achats peuvent être éffectué en toute sécurité</p>
+        <img src="asset/img_footer/security.svg" alt="paiement sécurisé">
+      </div>
+      <div class="footer_card">
+        <h4>Livraison sécurisé</h4>
+        <p>Vos commandes vous sont livrés en toute sécurité</p>
+        <img src="asset/img_footer/truck.svg" alt="paiement sécurisé">
+      </div>
+      <div class="footer_card">
+        <h4>Site certifié</h4>
+        <p>Nos produits sont certifiés par des professionnels</p>
+        <img src="asset/img_footer/medal.svg" alt="paiement sécurisé">
+      </div>
+      <div class="footer_card">
+        <h4>Nous contacter</h4>
+        <p>Contacter nos experts afin d'avoir plus d'informations</p>
+        <img src="asset/img_footer/chat.svg" alt="paiement sécurisé">
       </div>
 </footer>
 </div>
