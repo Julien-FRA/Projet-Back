0ad897cea7e44aebc99da07a63a51fe0623a71b2
FRANCOIS Julien <julien.francois@hetic.net>
2021-03-12 17:48:52 +0100

    inscription

--- config/init.php
+++ config/init.php
@@ -1,9 +1,96 @@
 <?php
 
-//connexion PDO
+require 'function.php';
 
+// Connexion à la bdd
+define('HOSTNAME', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'locky');
+
+$dsn = 'mysql:host=' . HOSTNAME . ';dbname=' . DATABASE;
+
+try { // on essai ce code
+    $pdo = new PDO($dsn,USERNAME,PASSWORD);
+} catch(PDOException $e) { // en cas d'erreur on la capture
+    die('<ul><li>Erreur sur le fichier : '. $e->getFile() . '</li><li>Erreur à la ligne ' . $e->getLine() . '</li><li>Message derreur : ' . $e->getMessage() . '</li></ul>');
+}
+
+$content = "";
+
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+
+if(isset($_POST['envoyer']) && $_POST['envoyer'] == 'Envoyer') {
+
+    $erreur = false;
+
+    if (isset($_POST['name']) && ($_POST['name'] !==''))  {
+        $name = htmlspecialchars($_POST['name']);
+      } else {
+        $content .= '<div>Le nom doit être remplit ! </div>';
+        $erreur = true;
+      }
+
+    if (isset($_POST['email']) && ($_POST['email'] !==''))  {
+        $email = htmlspecialchars($_POST['email']);
+      } else {
+        $content .= '<div>Le mail doit être remplit ! </div>';
+        $erreur = true;
+      }
+      
+    if (isset($_POST['cd']) && ($_POST['cd'] !==''))  {
+        if((iconv_strlen($_POST['cd']) == 5)) {
+            $cd = htmlspecialchars($_POST['cd']);
+        } else {
+            $content .= '<div>Le code postal doit être de 5 chiffres ! </div>';
+        }
+      } else {
+        $content .= '<div>Le code postal doit être remplit ! </div>';
+        $erreur = true;
+      } 
+
+    if (isset($_POST['tel']) && ($_POST['tel'] !==''))  {
+        if((iconv_strlen($_POST['tel']) == 10)) {
+            $tel = htmlspecialchars($_POST['tel']);
+        } else {
+            $content .= '<div>Le numéro de téléphone doit être de 10 chiffres ! </div>';
+        }
+      } else {
+        $content .= '<div>Le numéro de téléphone doit être remplit ! </div>';
+        $erreur = true;
+      }
+
+    if (isset($_POST['password']) && ($_POST['password'] !=='')) {
+        if(preg_match('/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[%!?*]).{10,20}$/', $_POST['password'])) {
+            $password = password_hash(htmlspecialchars($_POST['password']), PASSWORD_DEFAULT);
+        } else {
+            $content .= '<div>Le mot de passe est invalide, il doit contenir 1 lettre minuscule, 1 lettre majuscule, 1 chiffre, 1 caractère spécial(%!?*)</div>';
+            $erreur = true;
+        }
+      } else {
+        $content .= '<div>Le mot de passe doit être remplit ! </div>';
+        $erreur = true;
+      }
+
+    if (isset($_POST['genre']) && ($_POST['genre'] !==''))  {
+        $genre = htmlspecialchars($_POST['genre']);
+      } else {
+        $content .= '<div>Le numéro de téléphone doit être remplit ! </div>';
+        $erreur = true;
+      }
+
+    if (empty($content) && $erreur == false) {
+
+        addClient($pdo, $name, $email, $cd, $tel, $password, $genre);
+
+        $content = '<div class="succes">Bravo giga bg, tu es inscris</div>';
+
+        header("Refresh: 3;URL=login.php");
+    }
+}
 //variable d'affichage etc. 
 
 //constantes système 
 
-require 'function.php';
--- inscription.php
+++ inscription.php
@@ -4,13 +4,25 @@
 </header>
 <main>
 <section class="sign">
-        <form action="" class="signup">
+        <form action="" class="signup" method="post">
             <h1>Inscrivez-vous</h1>
-            <input type="name" name="name" id="name" class="inpt" required="required" placeholder="Your name">      
-            <input type="email" name="email" id="email" class="inpt" required="required" placeholder="Your email">
-            <input type="password" name="password" id="password" class="inpt" required="required" placeholder="Your password">
+            <?= $content; ?>
+            <input type="name" name="name" id="name" class="inpt" placeholder="Your name">      
+            <input type="email" name="email" id="email" class="inpt" placeholder="Your email">
+            <input type="number" name="cd" id="cd" class="inpt" placeholder="Your postal code">
+            <input type="number" name="tel" id="tel" class="inpt" placeholder="Your phone number">   
+            <input type="password" name="password" id="password" class="inpt" placeholder="Your password">
+            <input type="password" name="verifypassword" id="password" class="inpt" placeholder="Verify password">
+            <div class="sign-genre">
+                <label for="homme">Homme</label>
+                <input type="radio" name="genre" id="homme" class="inpt" value="homme">
+                <label for="femme">Femme</label>
+                <input type="radio" name="genre" id="femme" class="inpt" value="femme">
+                <label for="autre">Autres</label>
+                <input type="radio" name="genre" id="autre" class="inpt" value="autre">
+            </div> 
             <div class="submit-wrap">
-                <input type="submit" value="Envoyer" class="submit">
+                <input type="submit" value="Envoyer" name="envoyer" class="submit">
                 <a href="#" class="more">Termes et conditions</a>
             </div>
         </form>
