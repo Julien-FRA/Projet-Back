36467ece77d8615e842c87adaaf715b16a6bd569
FRANCOIS Julien <julien.francois@hetic.net>
2021-03-16 20:47:21 +0100

    admin

---
+++ admin.php
@@ -0,0 +1,9 @@
+<?php include 'config/template/head.php'; ?>
+<header>
+    <?php include 'config/template/nav.php'; ?>
+</header>
+<main>
+
+</main>
+
+<?php include 'config/template/footer.php'; ?>
