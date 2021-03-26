9528ba4d855db44400a4b84173095022a334df21
FRANCOIS Julien <julien.francois@hetic.net>
2021-03-26 10:00:41 +0100

    dashboard

---
+++ server/admin/delete.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once('../config/bdd.php');
+
+if(isset($_GET['id'])) {
+    $stmt = $pdo->prepare('SELECT * FROM produits WHERE id_produit=?');
+    $stmt->bindParam(1, $_GET['id'], PDO::PARAM_INT);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($row)
+{
+    $stmt = $pdo->prepare('DELETE FROM produits WHERE id_produit=?');
+    $stmt->bindParam(1, $_GET['id'], PDO::PARAM_INT);
+    $stmt->execute();
+    echo 'Votre produit à été supprimé';
+    header( "refresh:3;url=../../admin.php");
+}  
+} else {
+    http_response_code(404);
+}
+
+?>
